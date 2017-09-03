85e25fb7ef24bb0c376d3802dff11d9fe02660c9
jay <517642043@qq.com>
2017-09-03 14:59:22 +0800

    save

--- public/js/test.js
+++ public/js/test.js
@@ -6,3 +6,34 @@
 //         console.log(data.data.name);
 //     }
 // });
+
+// document.getElementById("fa").addEventListener("click",function () {
+//     console.log("fa bh");
+// }, true);
+// document.getElementById("son").addEventListener("click",function () {
+//     console.log("son bh");
+// }, true);
+// document.getElementById("fa").addEventListener("click",function () {
+//     console.log("fa mp");
+// }, false);
+// document.getElementById("son").addEventListener("click",function () {
+//     console.log("son mp");
+// }, true);
+document.getElementById("goHome").onclick =  function () {
+    console.log("click");
+    // return false
+    // event.preventDefault();
+    event.stopPropagation();
+}
+function fn (type) {
+    console.log("this is " + type)
+}
+// $("#goHome").live("click",function () {
+//     console.log("this is live")
+// });
+$("#fa").delegate("#son","click",function de () {
+    console.log("this is delegate")
+});
+$("#fa").on("click","#son",function o () {
+    console.log("this is on")
+});
