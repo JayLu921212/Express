eb3e39cf00e46c0cc4f8520b45507f5f3308b021
Jay Lu <jay.lu@cnsdc01.pwc.com>
2017-09-19 15:06:10 +0800

    save

--- public/js/dragAndDrop.js
+++ public/js/dragAndDrop.js
@@ -6,14 +6,31 @@ function dragOver()
 function drag()
 {
 	// debugger
-	event.dataTransfer.setData('URL',event.target.id);
+	event.dataTransfer.setData('Text',event.target.id);
 	// debugger
 }
 
 function drop()
 {
 	event.preventDefault();
-	var data=event.dataTransfer.getData('URL');
-	debugger
-	event.target.appendChild(document.getElementById(data));
+	var data= event.dataTransfer.getData('Text');
+	var div = document.createElement('div');
+	// div.id = 'zxczxc';
+	div.className = 'drop-div';
+	div.innerText = "divvvv";
+	div.style.margin = '20px 30px';
+	if (event.target.className === 'content') {
+		event.target.appendChild(div);
+	} else {
+		document.getElementById('qwe').insertBefore(div, event.target);
+	}
+}
+
+function dragOverPrevent () {
+	console.log("zxc")
+	// return true
+}
+
+function dropPrevent () {
+	console.log("zxc")
 }
